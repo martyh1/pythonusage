4c697119122953072845b3b8b24b0dc99abfae42
Marty Hammel <marty.hammel@gmail.com>
2018-11-12 11:57:43 -0700

    first commit

---
+++ scriptusageExample.py
@@ -0,0 +1,39 @@
+#sys for command line parms and 'quit'
+import sys 
+
+def printUsage():
+    print("usage: fileOrganize [-y][-t][-v]\n\t-y make changes\n\t-t tell changes that *would* be made.\n\t-v verbose")
+
+
+# tell user must use parms (so this doesn't just make changes to file system right away)
+if (len(sys.argv) == 1) or (len(sys.argv) > 4):
+    print("Invalid number of arguments")
+    printUsage()
+    quit()
+
+# save args in readable var names
+makeChanges = tellChangesOnly = verbose = False
+for arg in sys.argv:
+    if arg == '-y':
+        makeChanges = True
+    elif arg == '-t':
+        tellChangesOnly = True
+    elif arg == '-v':
+        verbose = True
+
+if not makeChanges and not tellChangesOnly:
+    print("invalid arguments.  Must use either -t or -y")
+    printUsage()
+    quit()
+
+#check for conflicting args
+if makeChanges and tellChangesOnly:
+    print("Cannot use -y and -t together.")
+    printUsage()
+    quit()
+
+#print selections if verbose
+if verbose:
+    print('Your selections:')
+    [print(arg) for arg in sys.argv if arg != sys.argv[0]]
+
